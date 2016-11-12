e14f254ac52dbdeeb7e09d1c73e827c4b2a83e55
Charles Khong <khong.ck.charles@gmail.com>
2016-11-12 01:21:09 -0700

    Created DatabaseInstance, added *.iml to gitignore

--- server/src/main/scala/BasicServer.scala
+++ server/src/main/scala/BasicServer.scala
@@ -1,30 +1,45 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
+import org.mongodb.scala.{MongoClient, MongoDatabase}
 
 import scala.io.StdIn
 
-object BasicServer extends App {
-  implicit val actorSystem = ActorSystem("hacktheu-system")
-  implicit val actorMaterializer = ActorMaterializer()
-
-  val interface = "localhost"
-  val port = 8080
-
-  import akka.http.scaladsl.server.Directives._
-
-  val route = {
-    pathEndOrSingleSlash {
-      complete("under construction")
+object BasicServer {
+  def main(args: Array[String]): Unit = {
+    if (args.length != 2) {
+      throw new IllegalArgumentException("Expected 2 arguments. Got: " + args.length)
     }
+
+    val databaseIP = args(0)
+    val database = args(1)
+    val mongoDatabase = new DatabaseInstance(databaseIP, database)
+
+
+    implicit val actorSystem = ActorSystem("hacktheu-system")
+    implicit val actorMaterializer = ActorMaterializer()
+
+    val interface = "localhost"
+    val port = 8080
+
+    import akka.http.scaladsl.server.Directives._
+
+    val route = {
+      pathEndOrSingleSlash {
+        complete("under construction")
+      }
+    }
+
+    val binding = Http().bindAndHandle(route, interface, port)
+    println(s"Server is now online at http://$interface:$port\nPress RETURN to stop...")
+    StdIn.readLine()
+
+    import actorSystem.dispatcher
+
+    binding.flatMap(_.unbind()).onComplete(_ => actorSystem.shutdown())
+    println("Server is shut down...")
   }
 
-  val binding = Http().bindAndHandle(route, interface, port)
-  println(s"Server is now online at http://$interface:$port\nPress RETURN to stop...")
-  StdIn.readLine()
-
-  import actorSystem.dispatcher
-
-  binding.flatMap(_.unbind()).onComplete(_ => actorSystem.shutdown())
-  println("Server is shut down...")
 }
+
+
