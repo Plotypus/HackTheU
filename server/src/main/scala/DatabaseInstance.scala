c067dd012dbb594be16e2d921f03dd836cc412c6
Pierce Darragh <pierce.darragh@gmail.com>
2016-11-12 18:50:14 -0700

    Backend changes
    :(

--- server/src/main/scala/DatabaseInstance.scala
+++ server/src/main/scala/DatabaseInstance.scala
@@ -24,32 +24,101 @@ class DatabaseInstance(val address: String, val db: String) {
     new ObjectId(s)
   }
 
-  def getUniqueFromCollectionWithAttributeById(collection: MongoCollection[Document], attribute: String, id: String) = {
-    Await.result(collection.find(equal(attribute, objectIdForString(id))).first().head(), atMostDuration)
+  def getUniqueFromCollectionWithAttributeValue(collection: MongoCollection[Document], attribute: String, value: Any) = {
+    Await.result(collection.find(equal(attribute, value)).first().head(), atMostDuration)
   }
 
-  def getAllFromCollectionWithAttributeById(collection: MongoCollection[Document], attribute: String, id: String) = {
-    Await.result(collection.find(equal(attribute, objectIdForString(id))).toFuture(), atMostDuration)
+  def getAllFromCollectionWithAttributeValue(collection: MongoCollection[Document], attribute: String, value: Any) = {
+    Await.result(collection.find(equal(attribute, value)).toFuture(), atMostDuration)
+  }
+
+  def getAttributeAsStringFromDocument(document: Document, attribute: String): String = {
+    document.get(attribute) match {
+      case Some(result) => result.asString().getValue
+      case None => ""
+    }
   }
 
   def getUser(id: String) = {
-    getUniqueFromCollectionWithAttributeById(users, "_id", id)
+    getUniqueFromCollectionWithAttributeValue(users, "_id", objectIdForString(id))
   }
 
   def getListing(id: String) = {
-    getUniqueFromCollectionWithAttributeById(listings, "_id", id)
+    getUniqueFromCollectionWithAttributeValue(listings, "_id", objectIdForString(id))
   }
 
   def getChat(id: String) = {
-    getUniqueFromCollectionWithAttributeById(chats, "_id", id)
+    getUniqueFromCollectionWithAttributeValue(chats, "_id", objectIdForString(id))
   }
 
   def getListingsForUser(listerId: String) = {
-    getAllFromCollectionWithAttributeById(listings, "lister", listerId)
+    getAllFromCollectionWithAttributeValue(listings, "lister", listerId)
   }
 
-  def addUser(username: String, password: String, email: String): String = {
+  def getListingsInUserLocation(userId: String) = {
+    val user = getUser(userId)
+    val location = getAttributeAsStringFromDocument(user, "location")
+    getAllFromCollectionWithAttributeValue(listings, "location", location)
+  }
 
+  def addUserToListing(userId: String, listingId: String) = {
+    val listing = getListing(listingId)
+    val arr = listing.get("interested_users") match {
+      case Some(result) =>
+        result.asArray().add(BsonObjectId(userId))
+        result
+      case None => throw new Exception("Couldn't find interested_users for listing")
+    }
+    listings.updateOne(equal("_id", listingId), Document("$set" -> Document("interested_users" -> arr)))
+  }
+
+  def addListingToUserInterests(listingId: String, userId: String) = {
+    val user = getUser(userId)
+
+    val arr = user.get("desired_pets") match {
+      case Some(result) =>
+        result.asArray().add(BsonObjectId(listingId))
+        result
+      case None => throw new Exception("Couldn't find desired_pets for user")
+    }
+    users.updateOne(equal("_id", objectIdForString(userId)), Document("$set" -> Document("desired_pets" -> arr)))
+  }
+
+  def createListing(userId: String, name: String, species: String, age: String, breed: String, weight: String) = {
+    println("createListing")
+    val user = getUser(userId)
+    println("get location")
+    val location = getAttributeAsStringFromDocument(user, "location")
+    println("generating id")
+    val original_id = new ObjectId()
+    val id = original_id.toString
+    println(s"id generated: $id")
+
+    println("creating document")
+
+    val doc: Document = Document(
+      "_id" -> id,
+      "lister" -> objectIdForString(userId),
+      "name" -> name,
+      "species" -> species,
+      "age" -> age,
+      "breed" -> breed,
+      "weight" -> weight,
+      "location" -> location,
+      "interested_users" -> BsonArray()
+    )
+
+    println("adding document")
+    Await.result(listings.insertOne(doc).toFuture(), atMostDuration)
+
+    id
+  }
+
+  def registerAndGetId(username: String, password: String, location: String): String = {
+
+    println(s"register request: $username, $password, $location")
+
+    //TODO: This should probably be checked... because we might get collisions, unless they are guaranteed unique.
     val original_id = new ObjectId()
     val id = original_id.toString
 
@@ -57,68 +126,33 @@ class DatabaseInstance(val address: String, val db: String) {
       "_id" -> id,
       "username" -> username,
       "password" -> password,
-      "email" -> email,
-      "listings" -> "[]", // List of listing_ids (personal listings)
-      "desired_pets" -> "[]", // List of listing_ids
-      "rejected_pets" -> "[]", // List of listing_ids
-      "chats" -> "[]" // List of chat_ids
+      "location" -> location,
+      "listings" -> BsonArray(),      // List of listing_ids (personal listings)
+      "desired_pets" -> BsonArray(),  // List of listing_ids
+      "rejected_pets" -> BsonArray(), // List of listing_ids
+      "chats" -> BsonArray()          // List of chat_ids
     )
-    val collection = database.getCollection("users")
-    collection.insertOne(doc).subscribe(
-      (blah: Completed) => println(blah)
-    )
+    Await.result(listings.insertOne(doc).toFuture(), atMostDuration)
 
     id
   }
 
-  def addListing(petName: String, listerID: UserID, petInfo: String) = {
-
+  def login(username: String, password: String): Option[String] = {
+    val user = getUniqueFromCollectionWithAttributeValue(users, "username", username)
+    val userpass = getAttributeAsStringFromDocument(user, "password")
+    if (userpass == password) {
+      Some(getAttributeAsStringFromDocument(user, "_id"))
+    } else {
+      None
+    }
   }
 
-  def addChat(lister: UserID, interested: UserID, listing: ListingID) = {
-
+  def getInterestedListingsFromUser(userId: String) = {
+    val user = getUser(userId)
+    user.get("desired_pets")
   }
-//
-//  def getItemFromDatabase(collection: MongoCollection[Document], id: String): String = {
-//    println(">> Finding ObjectId: " + id + " from " + collection.namespace)
-//    var itemJson = ""
-//    collection.find(equal("_id", id)).first().subscribe(
-//      (item: Document) => itemJson = item.toJson(), // onNext (success?)
-//      (error: Throwable) => println(s"Query failed: ${error.getMessage}"), // onError
-//      () => println("Done") // onComplete
-//    )
-//    itemJson
-//  }
 
   def closeConnection() = {
     client.close()
   }
-
-//  def listingFromDocument(document: Document): Listing = {
-//    val attributes = List("_id", "lister", "name", "species", "age", "breed", "weight", "interested_users", "location")
-//    val values = attributes.map(document.get)
-//    if (values.contains(None)) {
-//      println("[ERROR] Listing missing needed value")
-//    } else {
-//      new Listing(values(2))
-//    }
-//  }
-}
-
-abstract class ID(val id: ObjectId)
-
-case class UserID(override val id: ObjectId) extends ID(id) {}
-case class ListingID(override val id: ObjectId) extends ID(id) {}
-case class ChatID(override val id: ObjectId) extends ID(id) {}
-
-class User(val username: String, val password: String) {
-
-}
-
-class Listing(val petName: String, val listerID: UserID, val petInfo: String) {
-
-}
-
-class Chat(val lister: UserID, val interested: UserID, val listing: ListingID) {
-
 }
